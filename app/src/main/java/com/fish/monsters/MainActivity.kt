7e2c54fe5ff0f1dd5472f74e265ffbdd9b0fe7af
arix2000 <arix20003@gmail.com>
2023-10-25 19:34:10 +0200

    FM-1: add Home page

--- app/src/main/java/com/fish/monsters/MainActivity.kt
+++ app/src/main/java/com/fish/monsters/MainActivity.kt
@@ -3,15 +3,11 @@ package com.fish.monsters
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.fish.monsters.core.theme.FishMonstersTheme
+import com.fish.monsters.features.homeScreen.HomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,27 +15,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             FishMonstersTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    Greeting("Android")
+                    HomeScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Box {
-        Text(
-            text = "Hello $name!",
-            modifier = Modifier.align(Alignment.Center),
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FishMonstersTheme {
-        Greeting("Android")
     }
 }
