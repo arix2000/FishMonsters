e1b6db1074b17e64c3912abe047ecafc027c5fd5
Diana L <dlehmann@edu.cdv.pl>
2024-01-13 21:49:43 +0100

    Start Screen

--- app/src/main/java/com/fish/monsters/features/start/StartScreen.kt
+++ app/src/main/java/com/fish/monsters/features/start/StartScreen.kt
@@ -1,45 +1,157 @@
 package com.fish.monsters.features.start
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.QuestionMark
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.fish.monsters.R
-import com.fish.monsters.common.views.CapText
-import com.fish.monsters.common.views.buttons.FishButton
+import com.fish.monsters.common.models.ui.IconProps
+import com.fish.monsters.common.views.buttons.IconFishButton
 import com.fish.monsters.common.views.screenContent.ScreenBox
 import com.fish.monsters.core.navigation.Navigator
 import com.fish.monsters.core.navigation.Screen
+import com.fish.monsters.core.theme.LvlEasyColor
+import com.fish.monsters.core.theme.LvlHardColor
+import com.fish.monsters.core.theme.LvlMediumColor
 import com.fish.monsters.features.game.models.Difficulty
 import org.koin.compose.koinInject
 
 @Composable
 fun StartScreen(navigator: Navigator = koinInject()) {
     ScreenBox(title = stringResource(id = R.string.start)) {
-        Column {
-            FishButton(onClick = {
-                navigator.navigateTo(
-                    Screen.MainGameScreen,
-                    Difficulty.EASY.name
+        Column(
+            Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(5.dp)
+        ) {
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+
+                Image(
+                    painter = painterResource(R.drawable.level_easy),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(254.dp)
                 )
-            }) {
-                CapText(text = "easy")
+
+                Box(
+                    Modifier
+                        .fillMaxWidth()
+                        .height(258.dp)
+                        .background(LvlEasyColor)
+                        .clickable {
+                            navigator.navigateTo(
+                                Screen.MainGameScreen,
+                                Difficulty.EASY.name
+                            )
+                        },
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(text = "EASY")
+                }
+
+                IconFishButton(
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .padding(10.dp),
+                    iconProps = IconProps(icon = Icons.Default.QuestionMark),
+                    onClick = {
+
+                    })
             }
-            FishButton(onClick = {
-                navigator.navigateTo(
-                    Screen.MainGameScreen,
-                    Difficulty.NORMAL.name
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth().height(256.dp)
+            ) {
+
+                Image(
+                    painter = painterResource(R.drawable.level_medium),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .fillMaxSize()
+
                 )
-            }) {
-                CapText(text = "Normal")
+
+                Box(
+                    Modifier
+                        .fillMaxWidth()
+                        .height(260.dp)
+                        .background(LvlMediumColor)
+                        .clickable {
+                            navigator.navigateTo(
+                                Screen.MainGameScreen,
+                                Difficulty.NORMAL.name
+                            )
+                        },
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(text = "NORMAL")
+                }
+
+                IconFishButton(
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .padding(10.dp),
+                    iconProps = IconProps(icon = Icons.Default.QuestionMark),
+                    onClick = {
+
+                    })
             }
-            FishButton(onClick = {
-                navigator.navigateTo(
-                    Screen.MainGameScreen,
-                    Difficulty.HARD.name
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+
+                Image(
+                    painter = painterResource(R.drawable.level_hard),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .fillMaxSize()
                 )
-            }) {
-                CapText(text = "hard")
+
+                Box(
+                    Modifier
+                        .fillMaxWidth()
+                        .height(260.dp)
+                        .background(LvlHardColor)
+                        .clickable {
+                            navigator.navigateTo(
+                                Screen.MainGameScreen,
+                                Difficulty.HARD.name
+                            )
+                        },
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(text = "HARD")
+                }
+                IconFishButton(
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .padding(10.dp),
+                    iconProps = IconProps(icon = Icons.Default.QuestionMark),
+                    onClick = {
+
+                    })
             }
         }
     }
 }
+
